338f5dc203a2991445c0103b358123946ed2ae8e
DanayAlmazbekov <danay.almazbekov@gmail.com>
2022-06-07 11:40:00 +0600

    added post request

--- main.js
+++ main.js
@@ -10,3 +10,25 @@ GET  - получение данных
 команда для запуска json-server :
 json-server -w db.json 8000
 */
+/*
+CRUD - Create(POST) Read(GET) Update(PUT/PATCH) Delete(DELETE)
+*/
+const API = "http://localhost:8000/todo";
+
+
+let inpAdd = document.getElementById('inp-add');
+let btnAdd = document.getElementById('btn-add');
+// console.log(inpAdd, btnAdd);
+btnAdd.addEventListener('click', function () {
+  let newTodo ={
+    todo: inpAdd.value,
+  };
+  // console.log(newTodo);
+  fetch(API, {
+    method: 'POST',
+    body: JSON.stringify(newTodo),
+    headers: {
+      'Content-type': 'application/json; charset=utf-8'
+    },
+  });
+});
